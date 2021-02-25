204125add29f5b21b80ca0ade88cdcf545113223
김태영 <rlaxodud9809@khu.ac.kr>
2021-02-25 09:31:30 +0900

    프로젝트 구조 구성 및 필요 모듈 설치

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import AppRouter from 'components/Router';
+import { authService } from 'fbase';
+
+function App() {
+  console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()}</footer>
+    </>
+  );
+}
+
+export default App;
